757e08235b72ec5c65ed2d3d287adac99010b3cd
Luka <luka.vesa@gmail.com>
2026-01-25 19:31:42 +0200

    pari tehtävää

--- testitoimipliis.py
+++ testitoimipliis.py
@@ -1,14 +1,1 @@
-import random
 
-N = 100000
-n = 0
-pii = (4*n)/N
-arvo1 = 0
-arvo2 = 0
-
-arvo1 = random.uniform(-1,1)
-arvo2 = random.uniform(-1,1)
-
-kerta = float((arvo1 ** 2) + (arvo2 ** 2))
-
-print(kerta)
