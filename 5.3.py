3d217fc9a5995625ec1a2d63a3c4b8a54a28873b
Luka <luka.vesa@gmail.com>
2026-01-26 10:29:31 +0200

    pari tehtävää

--- 5.3.py
+++ 5.3.py
@@ -1,3 +1,16 @@
+#tee lista jossa on numerot annetun muuttujan ja yhden välillä
 luku = int(input("Anna luku ja kerron onko se alkuluku: "))
+lista = list(range(2,(luku)))
+n = luku
+check = 0
+#tee for, jossa tarkistetaan jos annettu luku on jaollinen listan numerolla.
 
-if luku in range()
+for luku in lista:
+    if n % luku == 0:
+        check += 1
+        break
+
+if check == 0:
+    print("Luku on alkuluku")
+else:
+    print("Luku ei ole alkuluku")
