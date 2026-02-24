912b8477c3c72c85830011436c15038924c471be
Luka <luka.vesa@gmail.com>
2026-02-24 11:44:33 +0200

    pari tehtävää

---
+++ 8.2.py
@@ -0,0 +1,20 @@
+import mysql.connector
+
+def hae_maan_lkm(iso_country):
+    sql = f"SELECT type, count(*) FROM airport where iso_country = '{iso_country}' group by type"
+    kursori = yhteys.cursor()
+    kursori.execute(sql)
+    tulos = kursori.fetchall()
+    if kursori.rowcount >0 :
+
+
+yhteys = mysql.connector.connect(
+         host='127.0.0.1',
+         port= 3306,
+         database='flight_game',
+         user='root',
+         password='Tekn11kan1hmelaps1',
+         autocommit=True
+         )
+
+hae_maa("KP")
